d53705b180e4c8bf55a77351d66f43a944417251
Md-Radwan <redwanhosen4@gmail.com>
2026-07-29 13:12:09 +0600

    add problem 4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Task 1 Problem statement:
+// Problem 1 statement:
 /*Write a function checkTemperature that takes a temperature (in Celsius) and returns "Hot" if it is 30 or above, "Cold" if it is 15 or below, and "Normal" otherwise.*/
 
 function checkTemperature(temp) {
@@ -18,7 +18,7 @@ console.log(checkTemperature(20)); // Expected: "Normal"
 
 
 
-// Task 2 Problem statement:
+// Problem 2 statement:
 /*Write a function reverseNumber that takes a positive whole number and returns it with its digits reversed. (Hint: convert the number to a string first.)*/
 
 function reverseNumber(num) {
@@ -35,7 +35,7 @@ console.log(reverseNumber(1234)); // Expected: 4321
 console.log(reverseNumber(7));    // Expected: 7
 
 
-// Task 3 Problem statement:
+// Problem 3 statement:
 /*Write a function getOddNumbers that takes a number n and returns an array of all odd numbers from 1 to n (inclusive).*/
 
 function getOddNumbers(n) {
@@ -47,4 +47,25 @@ function getOddNumbers(n) {
   return odds;
 }
  
-console.log(getOddNumbers(17));
+console.log(getOddNumbers(10));
+
+
+// Problem 4 statement:
+/*Write a function countVowels that takes a string and returns how many vowels it contains. Assume the string contains lowercase and uppercase letters, no spaces.*/
+
+function countVowels(str) {
+  let uppercaseStr = str;
+  let lowercaseStr = uppercaseStr.toLowerCase();
+  let vowels = "aeiou";
+  let count = 0;
+  for (let i = 0; i < lowercaseStr.length; i++) {
+    let letter = lowercaseStr[i];
+    if (vowels.includes(letter)) {
+      count++
+    }    
+  }
+  return count;
+}
+ 
+console.log(countVowels("RadwanhosEn"));
+// countVowels("Javascript")
