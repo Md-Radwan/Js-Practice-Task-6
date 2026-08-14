ab2fc18fab29f6c84c3d68b84ca50e8c07a32425
Md-Radwan <redwanhosen4@gmail.com>
2026-08-14 11:43:57 +0600

    problem 11 is added and solved

--- script.js
+++ script.js
@@ -186,3 +186,20 @@ function listKeys(obj) {
 }
  
 // console.log(listKeys({ name: "Sam", age: 25, city: "Dhaka" }));
+
+
+// Problem 12 statement: Write a function getValueOrDefault that takes an object, a key name, and a default value, and returns the object's value for that key if it exists, otherwise the default value.
+
+ function getValueOrDefault(obj, key, defaultValue) {
+  // TODO: check if the key exists in the object
+  if (Object.keys(obj).includes(key)) {
+    return obj[key]
+  } else {
+    return defaultValue
+  }
+ 
+}
+ 
+let person = { name: "Sam", age: 25 };
+console.log(getValueOrDefault(person, "age", 0));        // Expected: 25
+console.log(getValueOrDefault(person, "grade", "N/A"));  // Expected: "N/A"
